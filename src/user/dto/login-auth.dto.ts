96bb5fd9bcf3e6ee34f76b7b8cd950050907cedd
Pedro Marquez <pmarquez@grupoverfrut.local>
2022-10-22 08:46:24 -0500

    agregar validaciones al dto mensajes

--- src/user/dto/login-auth.dto.ts
+++ src/user/dto/login-auth.dto.ts
@@ -6,8 +6,8 @@ export class LoginAuthDto {
     @IsEmail()
     readonly correo: string;
 
-    @IsNotEmpty()
-    @IsString()
+    @IsNotEmpty({ message: "ingrese contraseña" })
+    @IsString({ message: "deberia ser una cadena" })
     @Length(6, 20, { message: "la contraseña debe tener como minimo 6 caracteres y como maximo 50" })
     readonly contrasena: string;
 }
